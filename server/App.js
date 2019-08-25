8459ba4b260668c51d43d3ac3589cc3f60480c68
Krzysztof Len <len.krzysztof@gmail.com>
2019-08-25 18:01:37 +0200

    [Server] Setup db connection

--- server/App.js
+++ server/App.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 
 const APP = express();
 
+// DB #######################
+require('./db/mongoose');
+
 APP.use((req, res, next) => {
     res.send({
         message: "Hello from server"
--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 5000;
 
 // LAUNCH ###################################################
 APP.listen(PORT, () => {
-    console.log(`=============App listening on port ${PORT}!=============`);
+    console.log(`============= App listening on port ${PORT} =============`);
 });
